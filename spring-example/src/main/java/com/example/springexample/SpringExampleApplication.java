6e58e8dc03cdf19b321525bc382f2384c1b060fd
Golde34 <nguyendongducviet2001@gmail.com>
2025-07-02 18:10:24 +0700

    add springboot example

--- spring-example/src/main/java/com/example/springexample/SpringExampleApplication.java
+++ spring-example/src/main/java/com/example/springexample/SpringExampleApplication.java
@@ -1,11 +1,13 @@
-package com.example.springlra;
+package com.example.springexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootLraApplication {
+public class SpringExampleApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootLraApplication.class, args);
+        SpringApplication.run(SpringExampleApplication.class, args);
     }
+
 }
